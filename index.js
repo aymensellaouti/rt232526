acccd9bb7f94198fe57c90c54bedfa3dcf0ed9a9
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-16 08:54:52 +0000

    seance du 13 2 2026 S3

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-console.log(1);
-setTimeout(() => {
-  console.log(2);
-}, 1000);
-console.log(3);
+// console.log(1);
+// setTimeout(() => {
+//   console.log(2);
+// }, 1000);
+// console.log(3);
 
-setTimeout(() => {
-  console.log(4);
-}, 500);
+// setTimeout(() => {
+//   console.log(4);
+// }, 500);
+//const ampoule = document.querySelector('.ampoule')
+// setInterval(
+//   () => {
+//     ampoule.classList.toggle('on')
+//     ampoule.classList.toggle('off')
+//   }, 1500
+// )
+let olRegions = document.querySelector('.regions');
+const regions  = ['Djerba', 'Soussa', 'Kasserine', 'Tatouin', 'Beja', 'Kef', 'Bizerte', 'Nabeul'];
+let index = 0;
+setInterval(
+  () => {
+    if (index == regions.length) {
+      olRegions.innerHTML = '';
+      index = 0;
+    }
+    const li = document.createElement('li');
+    li.innerHTML = regions[index++];
+    olRegions.insertBefore(li, olRegions.firstChild);
+  }, 1500
+)
