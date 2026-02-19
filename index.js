6f5da3fe57dca384e666b98d509c3b0bbedb5d92
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-19 11:10:27 +0100

    S4 Js events et nouveautés ES6  et plus

--- index.js
+++ index.js
@@ -7,7 +7,36 @@
 // setTimeout(() => {
 //   console.log(4);
 // }, 500);
-//const ampoule = document.querySelector('.ampoule')
+const ampoule = document.querySelector('.ampoule');
+ampoule.addEventListener('mouseenter', (e) => {
+  console.log(e);
+  ampoule.classList.toggle('on')
+  ampoule.classList.toggle('off')
+});
+const lis = document.querySelectorAll('li');
+const ol = document.querySelector('ol#numbers');
+const body = document.querySelector('body');
+body.addEventListener('dblclick', (e) => {
+  const nodeToDelete = e.target;
+  nodeToDelete.parentNode.removeChild(nodeToDelete);
+})
+//lis.forEach((li) => {
+  ol.addEventListener('click', (e)=> {
+    const li = e.target;
+    console.log({li});
+    
+    li.style.color = (li.style.color == 'red') ? 'green': 'red';
+  })
+//})
+
+a = document.querySelector('a');
+a.addEventListener('click', (e) => {
+  
+  
+  if (!confirm('sur t7ab tokhrej ?')) {
+    e.preventDefault();
+  }
+})
 // setInterval(
 //   () => {
 //     ampoule.classList.toggle('on')
@@ -17,14 +46,14 @@
 let olRegions = document.querySelector('.regions');
 const regions  = ['Djerba', 'Soussa', 'Kasserine', 'Tatouin', 'Beja', 'Kef', 'Bizerte', 'Nabeul'];
 let index = 0;
-setInterval(
-  () => {
-    if (index == regions.length) {
-      olRegions.innerHTML = '';
-      index = 0;
-    }
-    const li = document.createElement('li');
-    li.innerHTML = regions[index++];
-    olRegions.insertBefore(li, olRegions.firstChild);
-  }, 1500
-)
+// setInterval(
+//   () => {
+//     if (index == regions.length) {
+//       olRegions.innerHTML = '';
+//       index = 0;
+//     }
+//     const li = document.createElement('li');
+//     li.innerHTML = regions[index++];
+//     olRegions.insertBefore(li, olRegions.firstChild);
+//   }, 1500
+// )
